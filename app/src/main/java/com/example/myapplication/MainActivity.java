415b7a44f31385248a27725609845fc9db3bddf6
el-raziki <rmatu8326@gmail.com>
2020-12-10 07:19:42 -0800

    minute 20 to 40

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -17,10 +18,12 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     private int presCounter = 0;
-    private int maxPrescounter = 4;
+    private int maxPresCounter = 4;
     private String[] keys = {"R", "I", "B", "D", ""};
     private String textAnswer = "BIRD";
     TextView textScreen,textQuestion,textTitle;
+    Animation smallbigforth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
             addView(((LinearLayout) findViewById(R.id.layoutParent)), key, ((EditText) findViewById(R.id.editText)));
         }
 
-        maxPrescounter = 4;
+        maxPresCounter = 4;
     }
 
     private String[] shuffleArray(String[] ar) {
@@ -45,5 +48,80 @@ public class MainActivity extends AppCompatActivity {
             ar[i] = a;
         }
         return ar;
+    }
+
+    private void addView(LinearLayout viewParent, final String text, final EditText editText) {
+        LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        linearLayoutParams.rightMargin = 20;
+
+        final TextView textView = new TextView( this);
+
+        textView.setLayoutParams(linearLayoutParams);
+        textView.setBackground(this.getResources().getDrawable(R.drawable.bgpink));
+        textView.setTextColor(this.getResources().getColor(R.color.colorPurple));
+        textView.setGravity(Gravity.CENTER);
+        textView.setText(text);
+        textView.setClickable(true);
+        textView.setFocusable(true);
+        textView.setTextSize(32);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/FredokaOneRegular.ttf");
+
+        textQuestion = (TextView) findViewById(R.id.textQuestion);
+        textScreen = (TextView) findViewById(R.id.textScreen);
+        textTitle = (TextView) findViewById(R.id.textTitle);
+
+        textQuestion.setTypeface(typeface);
+        textScreen.setTypeface(typeface);
+        textTitle.setTypeface(typeface);
+        editText.setTypeface(typeface);
+        textView.setTypeface(typeface);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(presCounter < maxPresCounter) {
+                    if (presCounter==0) {
+                        editText.setText("");
+
+                        editText.setText(editText.getText().toString() + text);
+                        textView.startAnimation(smallbigforth);
+                        textView.animate().alpha(0).setDuration(300);
+                        presCounter++;
+
+                        if (presCounter == maxPresCounter)
+                            doValidate();
+
+                    }
+                }
+            }
+        });
+
+        viewParent.addView(textView);
+    }
+
+    private void doValidate() {
+        presCounter = 0;
+
+        EditText editText = findViewById(R.id.editText);
+        LinearLayout linearLayout = findViewById(R.id.layoutParent);
+
+        if(editText.getText().toString().equals(textAnswer)) {
+            Toast.makeText(MainActivity.this,"Correct", Toast.LENGTH_SHORT).show();
+            editText.setText("");
+        } else {
+            Toast.makeText(MainActivity.this, "Wrong", Toast.LENGTH_SHORT).show();
+            editText.setText("");
+        }
+
+        keys = shuffleArray(keys);
+        linearLayout.removeAllViews();
+        for (String key : keys) {
+            addView(linearLayout, key, editText);
+        }
     }
 }
