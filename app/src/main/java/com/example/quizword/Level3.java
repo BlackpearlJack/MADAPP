40496716cfc4faad67f81150660789cce47a1ec9
kingOyaro <dalton.oyaro@strathmore.edu>
2020-12-14 12:29:14 +0300

    Update on level 2 & 3

---
+++ app/src/main/java/com/example/quizword/Level3.java
@@ -0,0 +1,127 @@
+package com.example.quizword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class Level3 extends AppCompatActivity {
+
+
+
+    private int presCounter = 0;
+    private int maxPresCounter = 5;
+    private String[] keys = {"E", "R", "V", "R", "O", "I"};
+    private String textAnswer = "RIVER";
+    TextView textScreen,textQuestion,textTitle;
+
+
+     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level3);
+
+        keys = shuffleArray(keys);
+
+        for (String key : keys) {
+            addView(((LinearLayout) findViewById(R.id.layoutParent)), key, ((EditText) findViewById(R.id.editText)));
+        }
+
+        maxPresCounter = 5;
+    }
+
+    private String[] shuffleArray(String[] ar) {
+        Random rnd = new Random();
+        for (int i = ar.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            String a = ar[index];
+            ar[index] = ar[i];
+            ar[i] = a;
+        }
+        return ar;
+    }
+
+    private void addView(LinearLayout viewParent, final String text, final EditText editText) {
+        LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        linearLayoutParams.rightMargin = 20;
+
+        final TextView textView = new TextView( this);
+
+        textView.setLayoutParams(linearLayoutParams);
+        textView.setBackground(this.getResources().getDrawable(R.drawable.bgpink));
+        textView.setTextColor(this.getResources().getColor(R.color.colorPurple));
+        textView.setGravity(Gravity.CENTER);
+        textView.setText(text);
+        textView.setClickable(true);
+        textView.setFocusable(true);
+        textView.setTextSize(32);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/FredokaOne-Regular.ttf");
+
+        textQuestion = (TextView) findViewById(R.id.textQuestion);
+        textScreen = (TextView) findViewById(R.id.textScreen);
+        textTitle = (TextView) findViewById(R.id.textTitle);
+
+        textQuestion.setTypeface(typeface);
+        textScreen.setTypeface(typeface);
+        textTitle.setTypeface(typeface);
+        editText.setTypeface(typeface);
+        textView.setTypeface(typeface);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(presCounter < maxPresCounter) {
+                    if (presCounter==0)
+                        editText.setText("");
+
+                    editText.setText(editText.getText().toString() + text);
+                    textView.animate().alpha(0).setDuration(300);
+                    presCounter++;
+
+                    if (presCounter == maxPresCounter)
+                        doValidate();
+                }
+            }
+        });
+
+        viewParent.addView(textView);
+
+    }
+
+    private void doValidate() {
+        presCounter = 0;
+
+        EditText editText = findViewById(R.id.editText);
+        LinearLayout linearLayout = findViewById(R.id.layoutParent);
+
+        if(editText.getText().toString().equals(textAnswer)) {
+            Toast.makeText(Level3.this,"Correct", Toast.LENGTH_SHORT).show();
+            Intent a = new Intent(Level3.this,BossAct.class);
+            startActivity(a);
+
+        } else {
+            Toast.makeText(Level3.this, "Wrong", Toast.LENGTH_SHORT).show();
+        }
+        editText.setText("");
+
+        keys = shuffleArray(keys);
+        linearLayout.removeAllViews();
+        for (String key : keys) {
+            addView(linearLayout, key, editText);
+        }
+    }
+}
